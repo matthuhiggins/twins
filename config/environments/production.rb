46513995d1c4a05b95a5661be043ec71e4245f98
Matthew Higgins <developer@matthewhiggins.com>
2012-07-15 13:06:15 -0700

    moar

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,6 @@ Twins::Application.configure do
   # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
 
   # Enable threaded mode
   # config.threadsafe!
@@ -63,5 +62,4 @@ Twins::Application.configure do
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
-  # config.active_record.auto_explain_threshold_in_seconds = 0.5
 end
